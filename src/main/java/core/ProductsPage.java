2cd697f83d4705fd2a02d24cfbb9e1aa4faddbae
OlhaBilyk <o.bilyk888@gmail.com>
2020-01-23 11:45:02 +0200

    added some changes

--- src/main/java/core/ProductsPage.java
+++ src/main/java/core/ProductsPage.java
@@ -8,6 +8,9 @@ public class ProductsPage {
 
         //private By firstsearchProduct = By.xpath("//li[@class='product-item promo-product']//img[@class='img-product']");
         //private By producttype = By.xpath("//div[@class='active']//tr[2]//td[1]");
+
+
+     //   jtjut
     }
 
     public FirstProductPage firstsearchProduct (String productName){
